4b87e62d1cc9d1bfdc13beb2b1198e9387257e3b
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-01-25 22:16:30 +0200

    Feat: Add responsive footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,19 @@
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="h-12 md:h-16 text-red-500 flex justify-between items-center p-4 lg:p-20 xl:p-40">
+      <div className="flex-1 flex items-center">
+        <Link href="/" className="font-bold text-lg">
+          FlavorSync
+        </Link>
+      </div>
+      <div className="flex-1 flex items-center justify-end">
+        <p className="uppercase text-sm">&copy; All rights reserved</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
