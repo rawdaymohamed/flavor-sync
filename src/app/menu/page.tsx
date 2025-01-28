541c1665a2d9d7c013aa1d7fa171cb4ca82de789
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-01-28 17:16:53 +0000

    Fix: fix menu/ page style

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -3,26 +3,25 @@ import { menu } from "../data";
 import Link from "next/link";
 const MenuPage = () => {
   return (
-
-    <div className="flex flex-col md:flex-row justify-center md:items-center gap-4 h-[calc(100vh-6rem)] md:h-[calc(100vh-8rem)] p-4 lg:px-20 xl:px-40">
-    {/* Menu items */}
-    {menu.map((item) => (
-      <Link
-        href={`/menu/${item.slug}`}
-        key={item.id}
-        className="relative flex flex-col justify-end h-1/3 md:h-1/2 bg-cover bg-center rounded-lg overflow-hidden"
-        style={{ backgroundImage: `url(${item.img})` }}
-      >
-        <div
-          className="bg-black/50 text-white p-4 w-full"
-          style={{ color: `${item.color}` }}
+    <div className="flex flex-col md:flex-row justify-center md:items-center gap-2 h-[calc(100vh-6rem)] md:h-[calc(100vh-8rem)] p-4 lg:px-20 xl:px-40">
+      {/* Menu item 1 container */}
+      {menu.map((item) => (
+        <Link
+          href={`/menu/${item.slug}`}
+          key={item.id}
+          className="relative h-1/3 md:h-1/2 bg-cover"
+          style={{ backgroundImage: `url(${item.img})` }}
         >
-          <h1 className="font-bold text-lg md:text-2xl uppercase">{item.title}</h1>
-          <p className="text-sm md:text-base leading-relaxed">{item.desc}</p>
-        </div>
-      </Link>
-    ))}
-  </div>
+          <div
+            className={`flex flex-col gap-4 p-4 w-1/2`}
+            style={{ color: `${item.color}` }}
+          >
+            <h1 className="font-bold text-xl uppercase">{item.title}</h1>
+            <p className="text-sm">{item.desc}</p>
+          </div>
+        </Link>
+      ))}
+    </div>
   );
 };
 
