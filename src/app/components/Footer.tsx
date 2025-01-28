f646dac6a443eae5c97c06a562f7aa8804e4f94b
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-01-28 17:22:58 +0000

    Fix: fix footer style

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,12 +5,12 @@ const Footer = () => {
   return (
     <footer className="h-12 md:h-16 text-red-500 flex justify-between items-center p-5 lg:px-20 xl:px-40">
       <div className="flex-1 flex items-center">
-        <Link href="/" className="font-bold text-lg">
+        <Link href="/" className="font-bold">
           FlavorSync
         </Link>
       </div>
       <div className="flex-1 flex items-center justify-end">
-        <p className="uppercase text-sm">&copy; All rights reserved</p>
+        <small className="uppercase text-xs">&copy; All rights reserved</small>
       </div>
     </footer>
   );
