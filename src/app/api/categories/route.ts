255407a5eada919200b06168d41cc08ede3dbdaf
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-04-11 16:43:41 +0200

    Fix: fix prisma schema

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import prisma from "@/lib/prisma";
+import {prisma} from "@/lib/prisma";
 
 // Fetch all categories
 export const GET = async () => {
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,9 @@
-import { PrismaClient } from '../../generated/prisma'
-import { withAccelerate } from '@prisma/extension-accelerate'
+import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = global as unknown as { 
-    prisma: PrismaClient
-}
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
-
-export default prisma
