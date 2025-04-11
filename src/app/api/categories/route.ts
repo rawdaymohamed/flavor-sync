4e675154e70a0dcd4bc70c43651d5c77779ba7c1
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-04-11 13:57:04 +0200

    Feat: Add categories get all

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,4 +1,16 @@
 import { NextResponse } from "next/server";
-export const GET = () => {
-  return new NextResponse("Hello world", { status: 200 });
+import prisma from "@/lib/prisma";
+
+// Fetch all categories
+export const GET = async () => {
+  try {
+    const categories = await prisma.category.findMany();
+    return new NextResponse(JSON.stringify(categories), { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong" }),
+      { status: 500 }
+    );
+  }
 };
