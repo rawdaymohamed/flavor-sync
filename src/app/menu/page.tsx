7202c5d671713c0865889b2f497545cf9a2ba508
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-04-11 14:06:04 +0200

    Feat: make menu use dynamic data from backend

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
-import { menu } from "../data";
 import Link from "next/link";
-const MenuPage = () => {
+import { Menu } from "@/types/types";
+const getData = async () => {
+  const res = await fetch("http://localhost:3000/api/categories", {
+    cache: "no-cache",
+  });
+  if (!res.ok) throw new Error("Failed!");
+  return res.json();
+};
+const MenuPage = async () => {
+  const menu: Menu = await getData();
   return (
     <div className="flex flex-col md:flex-row justify-center md:items-center gap-2 h-[calc(100vh-6rem)] md:h-[calc(100vh-8rem)] p-4 lg:px-20 xl:px-40">
       {/* Menu item 1 container */}
@@ -10,7 +18,7 @@ const MenuPage = () => {
           href={`/menu/${item.slug}`}
           key={item.id}
           className="relative h-1/3 md:h-1/2 bg-cover"
-          style={{ backgroundImage: `url(${item.img})` }}
+          style={{ backgroundImage: `url(${item.imageURL})` }}
         >
           <div
             className={`flex flex-col gap-3 p-4 w-1/2`}
@@ -19,7 +27,9 @@ const MenuPage = () => {
             <h1 className="font-extrabold text-lg lg:text-xl uppercase">
               {item.title}
             </h1>
-            <p className="text-sm overflow-hidden lg:text-base">{item.desc}</p>
+            <p className="text-sm overflow-hidden lg:text-base">
+              {item.description}
+            </p>
           </div>
         </Link>
       ))}
---
+++ src/types/types.ts
@@ -0,0 +1,9 @@
+export type Menu= {
+  id: string;
+  createdAt: Date;
+  title: string;
+  description: string;
+  color: string;
+  imageURL: string;
+  slug: string;
+}[];
